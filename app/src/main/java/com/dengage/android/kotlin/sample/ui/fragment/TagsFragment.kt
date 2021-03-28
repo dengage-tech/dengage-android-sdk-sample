265ff26a769c4a04fb6c50e8f8acaa717026695d
Batuhan Coşkun <batu548@gmail.com>
2021-03-28 21:53:30 +0300

    Update set tag method

--- app/src/main/java/com/dengage/android/kotlin/sample/ui/fragment/TagsFragment.kt
+++ app/src/main/java/com/dengage/android/kotlin/sample/ui/fragment/TagsFragment.kt
@@ -1,14 +1,24 @@
 package com.dengage.android.kotlin.sample.ui.fragment
 
+import android.app.DatePickerDialog
 import com.dengage.android.kotlin.sample.R
 import com.dengage.android.kotlin.sample.databinding.FragmentTagsBinding
 import com.dengage.android.kotlin.sample.extensions.getDengageManager
 import com.dengage.android.kotlin.sample.ui.base.BaseDataBindingFragment
+import com.dengage.sdk.Constants
+import com.dengage.sdk.models.TagItem
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * Created by Batuhan Coskun on 24 March 2021
  */
 class TagsFragment : BaseDataBindingFragment<FragmentTagsBinding>() {
+
+    private val simpleDateFormat =
+        SimpleDateFormat(Constants.DATE_FORMAT, Locale.getDefault())
+    private var selectedChangeTime: Date? = null
+    private var selectedRemoveTime: Date? = null
 
     override fun getLayoutRes(): Int {
         return R.layout.fragment_tags
@@ -17,26 +27,54 @@ class TagsFragment : BaseDataBindingFragment<FragmentTagsBinding>() {
     override fun init() {
         sendPageView("tags")
 
+        binding.etChangeTime.setOnClickListener {
+            DatePickerDialog(
+                requireContext(),
+                { _, year, month, dayOfMonth ->
+                    val calendar = Calendar.getInstance()
+                    calendar.set(Calendar.YEAR, year)
+                    calendar.set(Calendar.MONTH, month)
+                    calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                    selectedChangeTime = calendar.time
+                    binding.etChangeTime.setText(simpleDateFormat.format(calendar.time))
+                }, 2021, 0, 1
+            ).show()
+        }
+
+        binding.etRemoveTime.setOnClickListener {
+            DatePickerDialog(
+                requireContext(),
+                { _, year, month, dayOfMonth ->
+                    val calendar = Calendar.getInstance()
+                    calendar.set(Calendar.YEAR, year)
+                    calendar.set(Calendar.MONTH, month)
+                    calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                    selectedRemoveTime = calendar.time
+                    binding.etRemoveTime.setText(simpleDateFormat.format(calendar.time))
+                }, 2021, 0, 1
+            ).show()
+        }
+
         binding.btnSend.setOnClickListener {
             val tag = binding.etTag.text.toString().trim()
             val value = binding.etValue.text.toString().trim()
-            val changeTime = binding.etChangeTime.text.toString().trim()
-            val changeValue = binding.etChangeValue.text.toString().trim()
-            val removeTime = binding.etRemoveTime.text.toString().trim()
+            val changeValue = binding.cbChangeValue.isChecked
 
-            val tags = mutableListOf<HashMap<String, String>>()
-            val tagItem = hashMapOf<String, String>()
-            tagItem["tag"] = tag
-            tagItem["value"] = value
-            if (changeTime.isNotEmpty()) {
-                tagItem["changeTime"] = changeTime
-            }
-            if (changeValue.isNotEmpty()) {
-                tagItem["changeValue"] = changeValue
-            }
-            if (removeTime.isNotEmpty()) {
-                tagItem["removeTime"] = removeTime
-            }
+            val tags = mutableListOf<TagItem>()
+            val tagItem = TagItem(
+                tag,
+                value,
+                selectedChangeTime,
+                changeValue,
+                selectedRemoveTime
+            )
+
+            // you can init TagItem with below parameters
+            /*val tagItem = TagItem(
+                tag,
+                value
+            )*/
+
             tags.add(tagItem)
             activity?.getDengageManager()?.setTags(tags)
         }
